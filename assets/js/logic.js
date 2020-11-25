c0cac510a02516c1979cf0e1c845e76a30a50944
Josh Herrera <joshherrera09@gmail.com>
2020-11-25 10:53:43 -0800

    finished logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -56,3 +56,97 @@ function getQuestion() {
         choicesEl.appendChild(choiceNode);
     });
 }
+
+function questionClick() {
+    // Check if user guessed wrong
+    if ( this.value !== questions[currentQuestionIndex].answer) {
+        // Penalize time
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+        feedbackEl.textContent = "Wrong!"
+        // Display new time on the page
+        timerEl.textContent = time;
+    } else {
+        feedbackEl.textContent = "Correct!";
+    }
+
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide")
+    }, 1000);
+
+
+    currentQuestionIndex ++;
+
+    // Check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+function quizEnd() {
+    // Stop timer
+    clearInterval(timerId);
+
+    // Show end screen
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.textContent = time;
+
+    // Hide questions section
+    questionsEl.setAttribute("class", "hide");
+}
+
+function clckTick() {
+    // Update time
+    time--;
+    timerEl.textContent = time;
+
+    // Check if user ran out of time
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+function saveHighscore() {
+    // Get value of input box
+    var initials = initialsEl.value.trim();
+
+    // Make sure value wasn't empty
+    if (initials !== "") {
+        // Get saved scores from localstorage, or if not any, set to empty array
+        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        // Format new score object for current user
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        // Save to local storage
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        // Redirect to next page
+        window.localStorage.href = "highscores.html";
+    }
+}
+
+function checkForEnter(event) {
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
+
+// User clicks button to submit inititals
+submitBtn.onclick = saveHighscore;
+
+// User clicks button to start quiz
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
